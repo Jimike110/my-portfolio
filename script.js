3085c0af524b23e7a5849fb235087f606e4aa395
Jimike <oladoyemike@gmail.com>
2023-07-27 18:16:01 +0100

    UPDATE: Reduced image sizes, removed unused assets, replaced SuyaLicious Image

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["WordPress Designer.", "Web Developer.", "Frontend Developer.", "Creative Person.", "vailable."],
-        typeSpeed: 60,
-        backSpeed: 40,
-        loop: true
-    });
+  $(".home .max-width .home-content a").click(function () {
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "WordPress Designer.",
+      "Web Developer.",
+      "Frontend Developer.",
+      "Creative Person.",
+      "vailable.",
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true,
+  });
 
   /*  var typed = new Typed(".typing-2", {
         strings: ["Developer", "Creative", "Designer"],
@@ -48,41 +58,41 @@ $(document).ready(function(){
         loop: true
     }); */
 
-// download popup script
-const downloadBtn = document.querySelector('#download-cv');
-const modal = document.querySelector('#modal');
-const closeBtn = document.querySelector('#close-btn');
+  // download popup script
+  const downloadBtn = document.querySelector("#download-cv");
+  const modal = document.querySelector("#modal");
+  const closeBtn = document.querySelector("#close-btn");
 
-downloadBtn.addEventListener('click', () => {
-  modal.style.display = 'block';
-});
+  downloadBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
 
-closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+  // owl carousel script
+  // $('.carousel').owlCarousel({
+  //     margin: 20,
+  //     loop: true,
+  //     autoplay: true,
+  //     autoplayTimeOut: 2000,
+  //     autoplayHoverPause: true,
+  //     responsive: {
+  //         0:{
+  //             items: 1,
+  //             nav: false
+  //         },
+  //         600:{
+  //             items: 2,
+  //             nav: false
+  //         },
+  //         1000:{
+  //             items: 3,
+  //             nav: false
+  //         }
+  //     }
+  // });
 });
 let year = document.getElementById("curYear");
-year.innerHTML = new Date().getFullYear()
+year.innerHTML = new Date().getFullYear();
